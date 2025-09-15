0fef579e17cb2295bf69ef393ed0b8ec310c3e37
failedman <2711339704@qq.com>
2025-09-15 19:17:15 +0800

    null

--- test.py
+++ test.py
@@ -1,10 +1,34 @@
 import torch
 import torch.utils.data as Data
 from captchaDataset import captchaDataset
+from torchvision  import transforms
+from PIL import Image
 import common
 
 
 if __name__ == '__main__':
+    ############################################################
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = torch.load("best.pth", map_location=device)
+    transform = transforms.Compose([
+    transforms.Resize((60, 160)),
+    transforms.Grayscale(),
+    transforms.ToTensor()
+    ])
+
+    img = Image.open("./dataset/test/0icv_1757934185.png")
+    input = transform(img).unsqueeze(0).to(device)
+    output = model(input)
+    output = output.view(-1, common.captcha_length, common.captcha_char.__len__())
+    output = torch.argmax(output, dim=2)[0]
+    
+    for i in range(common.captcha_length):
+        print(common.captcha_char[output[i]], end='')
+    print()
+
+
+    exit(0)
+    ############################################################
     test_data = captchaDataset("./dataset/test")
     test_dataloader = Data.DataLoader(dataset=test_data,
                                   batch_size=32,
@@ -33,3 +57,4 @@ if __name__ == '__main__':
             test_toal += targets.size(0)
     
     print("Test Accuracy: {:.2f}%".format(test_correct / test_toal * 100))
+
